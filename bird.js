1987d77f79c6cf6af81f17db2fb31aa3fbeb7c82
Toni Alatalo <toni@playsign.net>
2015-07-20 02:22:59 +0300

    head, tail, code refactor to have quads with the prev tri code, correct rotation (hack)

--- bird.js
+++ bird.js
@@ -14,25 +14,30 @@ function randomChoice(arr) {
     return arr[Math.floor(arr.length * Math.random())];
 }
 
-function createFace(facear) {
+function createFaceWithVertexColors(facear) {
     var posar = facear[0];
     var colar = facear[1];
     geometry = new THREE.Geometry()
+
+    for (var i = 0; i < posar.length; i++) {
+        var posdata = posar[i];
+        var vertex = new THREE.Vector3().fromArray(posdata);
+        geometry.vertices.push(vertex);
+    }
     
-    var vertex1 = new THREE.Vector3().fromArray(posar[0]);
-    var vertex2 = new THREE.Vector3().fromArray(posar[1]);
-    var vertex3 = new THREE.Vector3().fromArray(posar[2]);
-    geometry.vertices.push(vertex1);
-    geometry.vertices.push(vertex2);
-    geometry.vertices.push(vertex3);
     var face = new THREE.Face3(0, 1, 2);
-    geometry.faces.push( face );
-    geometry.computeBoundingSphere();
-            
+    geometry.faces.push(face);
+    if (geometry.vertices.length == 4) {
+        var face2 = new THREE.Face3(0, 3, 2); //http://stackoverflow.com/questions/16498296/three-js-face4-generates-triangle-instead-of-square
+        geometry.faces.push(face2);
+    }
+        
+    //geometry.computeBoundingSphere();
+
     for ( var i = 0; i < geometry.faces.length; i ++ ) {
 	f  = geometry.faces[ i ];
 
-        for( var j = 0; j < 3; j++ ) {
+        for( var j = 0; j < 3; j++ ) { //geometry.vertices.length
             var colhex = colar[j];
 	    var color = new THREE.Color(colhex);
 	    f.vertexColors[j] = color;
@@ -46,6 +51,8 @@ AN.Bird = function() {
     var headgeom = new THREE.SphereGeometry( 1, 32, 32 );
     var headmat  = new THREE.MeshBasicMaterial( {color: 0xa0a0a0} );
     this.head = new THREE.Mesh( headgeom, headmat ); //so can grow later, in eating thing
+    this.head.position.set(0.8, 0.6, 0);
+    this.head.scale.set(0.1, 0.1, 0.1);
         
     var radius = 1;
 
@@ -69,8 +76,8 @@ AN.Bird = function() {
         0x0000ff
     ]];
 
-    geometry1 = createFace(wingdata1);
-    geometry2 = createFace(wingdata2);
+    geometry1 = createFaceWithVertexColors(wingdata1);
+    geometry2 = createFaceWithVertexColors(wingdata2);
     geometry1.merge(geometry2);
     
     var material = new THREE.MeshBasicMaterial({
@@ -83,17 +90,49 @@ AN.Bird = function() {
     //for flapping wings in update()
     this.tip1 = geometry1.vertices[2];
     this.tip2 = geometry1.vertices[5];
-    this.wdir = -1;
 
-    this.vel = new THREE.Vector3();
-    this.target = null;
+    //tail, is simple static quad
+    /*py:       Vertex(self, -0.4,  0.0,  0.1),
+                Vertex(self, -0.9,  0.3,  0.3),                
+                Vertex(self, -0.9,  0.3, -0.3),
+                Vertex(self, -0.4,  0.0, -0.1) */
+    var taildata = [[
+        [-0.4,  0.0,  0],
+        [-0.9,  0.3,  0.3],
+        [-0.9,  0.3, -0.3],
+        [-0.4,  0.0, -0.1]
+   ], [
+       0xff0000,
+       0x00ff00,
+       0x0000ff,
+       0xff00ff
+    ]];
+    tailgeom = createFaceWithVertexColors(taildata);
+    geometry1.merge(tailgeom);
 
-    this.vel.x = 1;
+    var neckgeom = new THREE.Geometry();
+    var neckmat = new THREE.LineBasicMaterial({
+        color: headmat.color
+    });
+    neckgeom.vertices.push(this.head.position.clone());
+    neckgeom.vertices.push(new THREE.Vector3(
+        0.4, 0.0, 0.0
+    ));
+    var neck = new THREE.Line(neckgeom, neckmat);
+    //can't marge is is a Line, not Mesh - oops
+    //geometry1.merge(neckgeom);
+    //adding as a child below, like the head (but that's by design. this refactor to mesh perhaps)
 
     THREE.Mesh.call(this, geometry1, material);
-    this.add(this.head); //is not merged but separate for easy manip
-    this.head.position.set(0.8, 0.6, 0);
-    this.head.scale.set(0.1, 0.1, 0.1);
+
+    this.add(neck);
+
+    //head is not merged but separate for easy manip (grow when eats)
+    this.add(this.head); 
+
+    this.wdir = -1;
+    this.vel = new THREE.Vector3();
+    this.target = null;
 };
 
 AN.Bird.prototype = Object.create( THREE.Mesh.prototype );
@@ -107,6 +146,7 @@ AN.Bird.prototype.update = function () {
         this.target = new THREE.Vector3();
         //this.target = randomChoice(AN.food);
         this.lookAt(this.target);
+        this.rotateY(180); //oh well.
     }
 
     if (this.target != null) {
@@ -121,7 +161,7 @@ AN.Bird.prototype.update = function () {
             this.vel = d;
 
             //just testing here too
-            this.lookAt(this.target);
+            //this.lookAt(this.target);
         }
     }
 
