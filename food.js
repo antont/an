fdf7a6abb64e987179684a6ab7c74d3ee5e05ff2
Toni Alatalo <toni@playsign.net>
2015-08-10 21:28:06 +0300

    refactor apple to own object, again matching the original py version in the port. next to put effects of eating there

---
+++ food.js
@@ -0,0 +1,28 @@
+AN.Apple = function() {
+    var faceIndices = [ 'a', 'b', 'c' ];
+
+    var color, f, p, vertexIndex, radius = 40;
+    var geometry  = new THREE.IcosahedronGeometry( radius, 1 );
+
+    for ( var i = 0; i < geometry.faces.length; i ++ ) {
+	f  = geometry.faces[ i ];
+
+	for( var j = 0; j < 3; j++ ) {
+	    vertexIndex = f[ faceIndices[ j ] ];
+
+	    p = geometry.vertices[ vertexIndex ];
+
+	    color = new THREE.Color( 0xffffff );
+	    color.setHSL( ( p.y / radius + 1 ) / 2, 1.0, 0.5 );
+	    f.vertexColors[ j ] = color;
+	}
+    }
+
+    var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } )
+    //XXX note: should actually reuse the geometry and material!
+
+    THREE.Mesh.call(this, geometry, material);
+}
+
+AN.Apple.prototype = Object.create( THREE.Mesh.prototype );
+AN.Apple.prototype.constructor = AN.Apple;
