ad2ca5e1227564f05b62e52949220a074608baa9
Toni Alatalo <toni.alatalo@gmail.com>
2025-03-09 04:17:48 +0200

    current

--- up.sh
+++ up.sh
@@ -1,1 +1,16 @@
-gcloud storage cp index.html gs://an.org/
+export CLOUDSDK_CORE_PROJECT=playsign-151522
+
+# Upload HTML files with explicit content-type, charset, and encoding
+gcloud storage cp -Z \
+  --content-type="text/html; charset=utf-8" \
+  --content-encoding="utf-8" \
+  index.html gs://an.org/
+
+gcloud storage cp -Z \
+  --content-type="text/html; charset=utf-8" \
+  --content-encoding="utf-8" \
+  vaalit2025/index.html gs://an.org/vaalit2025/
+
+# Other static assets
+#gcloud storage cp -r at/ gs://an.org/
+gcloud storage cp -r images/ gs://an.org/
